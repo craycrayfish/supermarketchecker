097f53a0823fec974b3ccd536aa623f4d272e023
shawn <shawntan72@gmail.com>
2020-04-04 10:46:32 +0800

    Adding cloud sql connection

--- main.py
+++ main.py
@@ -1,8 +1,32 @@
 import os
 import telegram
+import sqlalchemy
 
 bot = telegram.Bot(token=os.environ["TELEGRAM_TOKEN"])
 
+db_user = os.environ.get("DB_USER")
+db_pass = os.environ.get("DB_PASS")
+db_name = os.environ.get("DB_NAME")
+cloud_sql_connection_name = os.environ.get("CLOUD_SQL_CONNECTION_NAME")
+
+db = sqlalchemy.create_engine(
+    # Equivalent URL:
+    # postgres+pg8000://<db_user>:<db_pass>@/<db_name>?unix_sock=/cloudsql/<cloud_sql_instance_name>/.s.PGSQL.5432
+    sqlalchemy.engine.url.URL(
+        drivername='postgres+pg8000',
+        username=db_user,
+        password=db_pass,
+        database=db_name,
+        query={
+            'unix_sock': '/cloudsql/{}/.s.PGSQL.5432'.format(
+                cloud_sql_connection_name)
+        }
+    ),
+    # ... Specify additional properties here.
+)
+
+def get_crowd_sizes(smIDs):
+    'SELECT crowd_size FROM '
 
 def webhook(request):
     if request.method == "POST":
